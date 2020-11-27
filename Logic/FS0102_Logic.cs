76bf71fb09ac8f4d33f77e41c3e55cf8bb03486c
uviwwer@163.com <uviwwer@163.com>
2020-11-27 16:37:58 +0800

    11

--- Logic/FS0102_Logic.cs
+++ Logic/FS0102_Logic.cs
@@ -19,7 +19,7 @@ namespace Logic
         }
 
 
-        #region 按检索条件检索,返回dt
+        #region 按检索条件检索,返回dt33
         public DataTable Search( string strRoleName)
         {
             return fs0102_DataAccess.Search(strRoleName);

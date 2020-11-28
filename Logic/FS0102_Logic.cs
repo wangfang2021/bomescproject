98187426b333bb3ee01d66c984e64acf328031df
fang wang <15822440710@163.com>
2020-11-28 09:06:31 +0800

    lo

--- Logic/FS0102_Logic.cs
+++ Logic/FS0102_Logic.cs
@@ -32,7 +32,7 @@ namespace Logic
         }
         #endregion
 
-        #region 验证角色名称是否被使用
+        #region 验证角色名称是否被使用 1
         public bool hasRoleName(string strRoleName)
         {
             return fs0102_DataAccess.hasRoleName(strRoleName);

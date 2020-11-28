a07fecb3d3647d67cf4a01d34e45abe0c403a859
hujun <uviwwer@163.com>
2020-11-28 09:04:12 +0800

    1

--- Logic/FS0102_Logic.cs
+++ Logic/FS0102_Logic.cs
@@ -25,7 +25,7 @@ namespace Logic
             return fs0102_DataAccess.Search(strRoleName);
         }
         #endregion
-        #region 检索所有机能 a
+        #region 检索所有机能 b
         public DataTable SearchRoleFunction()
         {
             return fs0102_DataAccess.SearchRoleFunction();

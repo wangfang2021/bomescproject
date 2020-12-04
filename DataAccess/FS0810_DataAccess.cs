c1ae0d5b963a402768da96db82a1fee01b41d963
fang wang <15822440710@163.com>
2020-12-04 16:31:56 +0800

    ddf

--- DataAccess/FS0810_DataAccess.cs
+++ DataAccess/FS0810_DataAccess.cs
@@ -90,5 +90,112 @@ namespace DataAccess
         }
         #endregion
 
+        #region 检索_品目
+        public DataTable Search_PM(string smallpm, string bigpm)
+        {
+            try
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.Append("select vcBigPM,vcSmallPM,'1' as vcaddflag,'1' as vcmodflag,vcBigPM as vcBigPM_init,vcSmallPM as vcSmallPM_init  \n");
+                strSql.Append("from tPMRelation   \n");
+                strSql.Append("where isnull(vcBigPM,'') like '%"+bigpm+"%' and isnull(vcSmallPM,'') like '%"+smallpm+"%'  \n");
+                return excute.ExcuteSqlWithSelectToDT(strSql.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region 保存_品目
+        public void Save_pm(DataTable dt, string strUserId)
+        {
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                for(int i=0;i<dt.Rows.Count;i++)
+                {
+                    DataRow dr = dt.Rows[i];
+                    if(dr["vcflag"].ToString()=="add")
+                    {
+                        sql.Append("insert into tPMRelation (vcBigPM,vcSmallPM) values   \n");
+                        sql.Append("('" + dr["vcBigPM"].ToString() + "','" + dr["vcSmallPM"].ToString() + "')  \n");
+                    }
+                    if(dr["vcflag"].ToString() == "mod")
+                    {
+                        sql.Append("update tPMRelation set vcBigPM='" + dr["vcBigPM"].ToString() + "',vcSmallPM='" + dr["vcSmallPM"].ToString() + "'   \n");
+                        sql.Append("where vcBigPM='" + dr["vcBigPM_init"].ToString() + "' and vcSmallPM='" + dr["vcSmallPM_init"].ToString() + "'   \n");
+                    }
+                }
+                if (sql.Length > 0)
+                {
+                    excute.ExcuteSqlWithStringOper(sql.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region 取得关系表中所有大品目
+        public DataTable GetBigPM()
+        {
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append("select distinct vcBigPM from tPMRelation  \n");
+                return excute.ExcuteSqlWithSelectToDT(sql.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region 取得关系表中所有ih品目
+        public DataTable GetSmallPM()
+        {
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append("select distinct vcSmallPM from tPMRelation  \n");
+                return excute.ExcuteSqlWithSelectToDT(sql.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region 删除_品目
+        public void Del_pm(DataTable dtdel, string strUserId)
+        {
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                for (int i = 0; i < dtdel.Rows.Count; i++)
+                {
+                    DataRow dr = dtdel.Rows[i];
+                    sql.Append("delete from tPMRelation   \n");
+                    sql.Append("where vcBigPM='" + dr["vcBigPM"].ToString() + "' and vcSmallPM='" + dr["vcSmallPM"].ToString() + "'   \n");
+                }
+                if (sql.Length > 0)
+                {
+                    excute.ExcuteSqlWithStringOper(sql.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
     }
 }
--- Logic/FS0810_Logic.cs
+++ Logic/FS0810_Logic.cs
@@ -38,5 +38,40 @@ namespace Logic
             fs0810_DataAccess.Del(dtdel, strUserId);
         }
         #endregion
+
+        #region 检索_品目
+        public DataTable Search_PM(string smallpm, string bigpm)
+        {
+            return fs0810_DataAccess.Search_PM(smallpm, bigpm);
+        }
+        #endregion
+
+        #region 保存_品目
+        public void Save_pm(DataTable dtadd, string strUserId)
+        {
+            fs0810_DataAccess.Save_pm(dtadd, strUserId);
+        }
+        #endregion
+
+        #region 取得关系表中所有大品目
+        public DataTable GetBigPM()
+        {
+            return fs0810_DataAccess.GetBigPM();
+        }
+        #endregion
+
+        #region 取得关系表中所有小品目
+        public DataTable GetSmallPM()
+        {
+            return fs0810_DataAccess.GetSmallPM();
+        }
+        #endregion
+
+        #region 删除_品目
+        public void Del_pm(DataTable dtdel, string strUserId)
+        {
+            fs0810_DataAccess.Del_pm(dtdel, strUserId);
+        }
+        #endregion
     }
 }

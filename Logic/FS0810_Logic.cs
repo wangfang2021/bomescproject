ebb7aac5cce0843ee75d620907687c191a4bcd5f
fang wang <15822440710@163.com>
2020-12-07 13:38:01 +0800

    wf

--- Logic/FS0810_Logic.cs
+++ Logic/FS0810_Logic.cs
@@ -26,9 +26,9 @@ namespace Logic
         #endregion
 
         #region 保存
-        public void Save(DataTable dtadd, DataTable dtmod,string strUserId)
+        public void Save(DataTable dtadd, DataTable dtmod, string strUserId)
         {
-            fs0810_DataAccess.Save(dtadd,dtmod, strUserId);
+            fs0810_DataAccess.Save(dtadd, dtmod, strUserId);
         }
         #endregion
 
@@ -71,6 +71,42 @@ namespace Logic
         public void Del_pm(DataTable dtdel, string strUserId)
         {
             fs0810_DataAccess.Del_pm(dtdel, strUserId);
+        }
+        #endregion
+
+        #region 检索_基准时间
+        public DataTable Search_StandardTime(string bigpm, string standardtime)
+        {
+            return fs0810_DataAccess.Search_StandardTime(bigpm, standardtime);
+        }
+        #endregion
+
+        #region 大品目、基准时间是否重复，返回true存在，false不存在
+        public bool IsExit(string bigpm, string standardtime)
+        {
+            int rows = fs0810_DataAccess.GetStandardTime(bigpm, standardtime);
+            if (rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region 保存_基准时间
+        public void Save_Standardtime(DataTable dtadd, string strUserId)
+        {
+            fs0810_DataAccess.Save_Standardtime(dtadd, strUserId);
+        }
+        #endregion
+
+        #region 删除_品目
+        public void Del_Standardtime(DataTable dtdel, string strUserId)
+        {
+            fs0810_DataAccess.Del_Standardtime(dtdel, strUserId);
         }
         #endregion
     }

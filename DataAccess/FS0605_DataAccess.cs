cf45a4dbb17312353e27bc6480c7f5337f81a1ab
niuruihua <964587145@qq.com>
2020-12-07 17:06:11 +0800

    FS0605

---
+++ DataAccess/FS0605_DataAccess.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Common;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace DataAccess
+{
+    public class FS0605_DataAccess
+    {
+        private MultiExcute excute = new MultiExcute();
+
+        /// <summary>
+        /// 验证新增的数据是否已经存在数据库
+        /// </summary>
+        /// <param name="dtadd"></param>
+        /// <returns></returns>
+        public bool isExistAddData(DataTable dtadd)
+        {
+            try
+            {
+                StringBuilder strSql = new StringBuilder();
+                for (int i = 0; i < dtadd.Rows.Count; i++)
+                {
+                    if (strSql.Length > 0)
+                    {
+                        strSql.AppendLine("  union all SELECT vcSupplier_id, vcWorkArea, vcIsSureFlag, vcLinkMan, vcPhone, vcEmail FROM [dbo].[TSupplierInfo] where vcSupplier_id='" + dtadd.Rows[i]["vcSupplier_id"] + "' and vcWorkArea='" + dtadd.Rows[i]["vcWorkArea"] + "'  ");
+                    }
+                    else
+                    {
+                        strSql.AppendLine("  SELECT vcSupplier_id, vcWorkArea, vcIsSureFlag, vcLinkMan, vcPhone, vcEmail FROM [dbo].[TSupplierInfo] where vcSupplier_id='" + dtadd.Rows[i]["vcSupplier_id"] + "' and vcWorkArea='" + dtadd.Rows[i]["vcWorkArea"] + "'  ");
+                    }
+                }
+                DataTable dt = excute.ExcuteSqlWithSelectToDT(strSql.ToString());
+                return dt.Rows.Count > 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// 检索数据
+        /// </summary>
+        /// <param name="typeCode"></param>
+        /// <returns></returns>
+        public DataTable Search(string vcSupplier_id, string vcWorkArea, string vcIsSureFlag)
+        {
+            try
+            {
+                StringBuilder strSql = new StringBuilder();
+
+                strSql.AppendLine("   select  [vcSupplier_id], [vcWorkArea], case when vcIsSureFlag='0' then '未确认' when vcIsSureFlag='1'  then '已确认' else '' end as vcIsSureFlag, [vcLinkMan], [vcPhone], [vcEmail], [vcOperatorID], [dOperatorTime],'1' as vcmodflag,'1' as vcaddflag From [dbo].[TSupplierInfo] where 1=1   ");
+
+                if (vcSupplier_id.Length > 0)
+                {
+                    strSql.AppendLine("  and  vcSupplier_id like '%" + vcSupplier_id + "%' ");
+                }
+                if (vcWorkArea.Length > 0)
+                {
+                    strSql.AppendLine("  and  vcWorkArea like '%" + vcWorkArea + "%' ");
+                }
+                if (vcIsSureFlag.Length > 0)
+                {
+                    strSql.AppendLine("  and  vcIsSureFlag = '" + vcIsSureFlag + "' ");
+                }
+                strSql.AppendLine("  order by  vcWorkArea ");
+                return excute.ExcuteSqlWithSelectToDT(strSql.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="dtdel"></param>
+        /// <param name="userId"></param>
+        public void Del(DataTable dtdel, string userId)
+        {
+            StringBuilder sql = new StringBuilder();
+            for (int i = 0; i < dtdel.Rows.Count; i++)
+            {
+                DataRow dr = dtdel.Rows[i];
+                sql.Append("delete from [TSupplierInfo]   \n");
+                sql.Append("where vcSupplier_id='" + dr["vcSupplier_id"].ToString() + "' and vcWorkArea='" + dr["vcWorkArea"].ToString() + "' \n");
+
+            }
+            if (sql.Length > 0)
+            {
+                excute.ExcuteSqlWithStringOper(sql.ToString());
+            }
+        }
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <param name="dtadd"></param>
+        /// <param name="userId"></param>
+        public void Save(DataTable dtadd, DataTable dtmod, string userId)
+        {
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                for (int i = 0; i < dtadd.Rows.Count; i++)
+                {
+                    DataRow dr = dtadd.Rows[i];
+                    sql.Append("insert into [TSupplierInfo] (vcSupplier_id, vcWorkArea, vcIsSureFlag, vcLinkMan, vcPhone, vcEmail, vcOperatorID, dOperatorTime)  \n");
+                    sql.Append(" values('" + dr["vcSupplier_id"].ToString() + "','" + dr["vcWorkArea"].ToString() + "','" + dr["vcIsSureFlag"].ToString() + "','" + dr["vcLinkMan"].ToString() + "','" + dr["vcPhone"].ToString() + "','" + dr["vcEmail"].ToString() + "','" + userId + "',GETDATE()) \n");
+                }
+                for (int i = 0; i < dtmod.Rows.Count; i++)
+                {
+                    DataRow dr = dtmod.Rows[i];
+                    sql.Append("update TSupplierInfo set vcWorkArea='" + dr["vcWorkArea"].ToString() + "', vcIsSureFlag='" + dr["vcIsSureFlag"].ToString() + "', vcLinkMan='" + dr["vcLinkMan"].ToString() + "', vcPhone='" + dr["vcPhone"].ToString() + "', vcEmail='" + dr["vcEmail"].ToString() + "',vcOperatorID='" + userId + "',dOperatorTime=GETDATE()  \n");
+                    sql.Append("where vcSupplier_id='" + dr["vcSupplier_id"].ToString() + "'   \n");
+
+                }
+                if (sql.Length > 0)
+                {
+                    excute.ExcuteSqlWithStringOper(sql.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 验证修改的数据是否已经存在数据库
+        /// </summary>
+        /// <param name="dtamod"></param>
+        /// <returns></returns>
+        public bool isExistModData(DataTable dtamod)
+        {
+            try
+            {
+                StringBuilder strSql = new StringBuilder();
+                for (int i = 0; i < dtamod.Rows.Count; i++)
+                {
+                    if (strSql.Length > 0)
+                    {
+                        strSql.AppendLine("  union all select vcSupplier_id, vcWorkArea, vcIsSureFlag, vcLinkMan, vcPhone, vcEmail, vcOperatorID, dOperatorTime from dbo.TSupplierInfo where vcSupplier_id='" + dtamod.Rows[i]["vcSupplier_id"] + "'  ");
+                    }
+                    else
+                    {
+                        strSql.AppendLine("  select vcSupplier_id, vcWorkArea, vcIsSureFlag, vcLinkMan, vcPhone, vcEmail, vcOperatorID, dOperatorTime from dbo.TSupplierInfo where vcSupplier_id='" + dtamod.Rows[i]["vcSupplier_id"] + "'  ");
+                    }
+                }
+                DataTable dt = excute.ExcuteSqlWithSelectToDT(strSql.ToString());
+                return dt.Rows.Count > 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}

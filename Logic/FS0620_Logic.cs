73d254ef56efc8329392e21d6f5a0946e5459bfb
niuruihua <964587145@qq.com>
2020-12-10 15:58:29 +0800

    FS0620

---
+++ Logic/FS0620_Logic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataAccess;
+using System.Collections;
+
+
+namespace Logic
+{
+    public class FS0620_Logic
+    {
+        FS0620_DataAccess fs0620_DataAccess;
+
+        public FS0620_Logic()
+        {
+            fs0620_DataAccess = new FS0620_DataAccess();
+
+        }
+
+        public DataTable Search(string vcTargetYear, string vcPartNo, string vcInjectionFactory, string vcInsideOutsideType, string vcSupplier_id, string vcWorkArea, string vcCarType)
+        {
+            return fs0620_DataAccess.Search(vcTargetYear, vcPartNo, vcInjectionFactory, vcInsideOutsideType, vcSupplier_id, vcWorkArea, vcCarType);
+        }
+
+        public bool isExistAddData(DataTable dtadd)
+        {
+            return fs0620_DataAccess.isExistAddData(dtadd);
+        }
+
+        public bool isExistModData(DataTable dtamod)
+        {
+            return fs0620_DataAccess.isExistModData(dtamod);
+        }
+
+        public DataTable BindInsideOutsideType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable BindInjectionFactory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

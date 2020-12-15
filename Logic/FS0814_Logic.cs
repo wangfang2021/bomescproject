16ef1d0c9f3cca340c2c2bd5a10e05ee36d36900
fang wang <15822440710@163.com>
2020-12-15 11:08:41 +0800

    dd

---
+++ Logic/FS0814_Logic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataAccess;
+using System.Collections;
+
+
+namespace Logic
+{
+    public class FS0814_Logic
+    {
+        FS0814_DataAccess fs0814_DataAccess;
+
+        public FS0814_Logic()
+        {
+            fs0814_DataAccess = new FS0814_DataAccess();
+        }
+
+        #region 检索
+        public DataTable Search(string strYearMonth)
+        {
+            return fs0814_DataAccess.Search(strYearMonth);
+        }
+        #endregion
+
+    }
+}
---
+++ SPPSApi/Controllers/G08/FS0814Controller.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Text.Json;
+using System.Threading;
+using Common;
+using Logic;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace SPPSApi.Controllers.G08
+{
+    [Route("api/FS0814/[action]")]
+    [EnableCors("any")]
+    [ApiController]
+    public class FS0814Controller : BaseController
+    {
+        FS0814_Logic fs0814_Logic = new FS0814_Logic();
+        private readonly string FunctionID = "FS0814";
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FS0814Controller(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        #region 检索
+        [HttpPost]
+        [EnableCors("any")]
+        public string searchApi([FromBody]dynamic data)
+        {
+            //验证是否登录
+            string strToken = Request.Headers["X-Token"];
+            if (!isLogin(strToken))
+            {
+                return error_login();
+            }
+            LoginInfo loginInfo = getLoginByToken(strToken);
+            //以下开始业务处理
+            ApiResult apiResult = new ApiResult();
+            dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
+            string strYearMonth = dataForm.vcYearMonth == null ? "" : dataForm.vcYearMonth;
+
+            try
+            {
+                DataTable dt = fs0814_Logic.Search(strYearMonth);
+                List<Object> dataList = ComFunction.convertAllToResult(dt);
+                apiResult.code = ComConstant.SUCCESS_CODE;
+                apiResult.data = dataList;
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+            catch (Exception ex)
+            {
+                ComMessage.GetInstance().ProcessMessage(FunctionID, "M01UE0204", ex, loginInfo.UserId);
+                apiResult.code = ComConstant.ERROR_CODE;
+                apiResult.data = "检索失败";
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+        }
+        #endregion
+
+    }
+}

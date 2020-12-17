d55c130369e53977a17cb536cbc5dc281e91e558
zhanghaipeng <arale22@163.com>
2020-12-17 09:06:40 +0800

    123

--- DataAccess/FS1310_DataAccess.cs
+++ DataAccess/FS1310_DataAccess.cs
@@ -12,12 +12,12 @@ namespace DataAccess
     public class FS1310_DataAccess
     {
         private MultiExcute excute = new MultiExcute();
-        public DataTable getDataInfo()
+        public DataTable getPlantInfo()
         {
             try
             {
                 StringBuilder strSql = new StringBuilder();
-                strSql.AppendLine("");
+                strSql.AppendLine("select '1' as Plantcode,'泰达' as Plantname");//Plantcode\Plantname
                 strSql.AppendLine("");
                 strSql.AppendLine("");
                 strSql.AppendLine("");
@@ -27,6 +27,66 @@ namespace DataAccess
                 strSql.AppendLine("");
                 strSql.AppendLine("");
                 return excute.ExcuteSqlWithSelectToDT(strSql.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public DataTable getPinMuInfo()
+        {
+            try
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.AppendLine("select '1' as PinMucode,'中小物' as PinMuname");//Plantcode\Plantname
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                return excute.ExcuteSqlWithSelectToDT(strSql.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public DataTable getSearchInfo(string strPlant, string strPinMu, string strPartNo)
+        {
+            try
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.AppendLine("select '' as dPlant,'' as dPartNo,'' as dPinMu");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                strSql.AppendLine("");
+                return excute.ExcuteSqlWithSelectToDT(strSql.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int setDeleteInfo(ArrayList delList)
+        {
+            try
+            {
+                StringBuilder strSql = new StringBuilder();
+                for (int i = 0; i < delList.Count; i++)
+                {
+                    strSql.AppendLine("");
+                    strSql.AppendLine("");
+                    strSql.AppendLine("");
+                }
+                return excute.ExcuteSqlWithStringOper(strSql.ToString());
             }
             catch (Exception ex)
             {
--- Logic/FS1310_Logic.cs
+++ Logic/FS1310_Logic.cs
@@ -16,9 +16,34 @@ namespace Logic
         {
             fs1310_DataAccess = new FS1310_DataAccess();
         }
-        public DataTable getDataInfo()
+        public DataTable getPlantInfo()
         {
-            return fs1310_DataAccess.getDataInfo();
+            return fs1310_DataAccess.getPlantInfo();
         }
+        public DataTable getPinMuInfo()
+        {
+            return fs1310_DataAccess.getPinMuInfo();
+        }
+        public DataTable getSearchInfo(string strPlant, string strPinMu, string strPartNo)
+        {
+            return fs1310_DataAccess.getSearchInfo(strPlant, strPinMu, strPartNo);
+        }
+        public bool setDeleteInfo(ArrayList delList)
+        {
+            try
+            {
+                //返回值定义，默认为失败false
+                bool bIsOK = false;
+                int count = fs1310_DataAccess.setDeleteInfo(delList);
+                if (count > 0)
+                    bIsOK = true;
+                return bIsOK;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }

e5f02e46802059521046c834303dfbddb9eac4b7
wangliwei <fqm_wlw@tftm.com.cn>
2021-01-05 17:06:02 +0800

    1

---
+++ SPPSApi/Controllers/G12/FS1205Controller.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Text.Json;
+using System.Threading;
+using Common;
+using Logic;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SPPSApi.Controllers.G12
+{
+    [Route("api/FS1205/[action]")]
+    [EnableCors("any")]
+    [ApiController]
+    public class FS1205Controller : BaseController
+    {
+        FS1205_Logic fS1205_Logic = new FS1205_Logic();
+        private readonly string FunctionID = "FS1205";
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FS1205Controller(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        #region 页面初始化
+        [HttpPost]
+        [EnableCors("any")]
+        public string pageloadApi()
+        {
+            string strToken = Request.Headers["X-Token"];
+            if (!isLogin(strToken))
+            {
+                return error_login();
+            }
+            LoginInfo loginInfo = getLoginByToken(strToken);
+            //以下开始业务处理
+            ApiResult apiResult = new ApiResult();
+            try
+            {
+                FS1211_Logic fS1211_Logic = new FS1211_Logic();
+                Dictionary<string, Object> res = new Dictionary<string, Object>();
+                List<Object> dataList_PlantSource = ComFunction.convertAllToResult(fS1205_Logic.bindplant());
+                List<Object> dataList_TypeSource = ComFunction.convertAllToResult(fS1205_Logic.getPlantype());
+                res.Add("PlantSource", dataList_PlantSource);
+                res.Add("TypeSource", dataList_TypeSource);
+
+                apiResult.code = ComConstant.SUCCESS_CODE;
+                apiResult.data = res;
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+            catch (Exception ex)
+            {
+                ComMessage.GetInstance().ProcessMessage(FunctionID, "M00UE0006", ex, loginInfo.UserId);
+                apiResult.code = ComConstant.ERROR_CODE;
+                apiResult.data = "初始化失败";
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+        }
+        #endregion
+
+    }
+}

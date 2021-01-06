7c4b20cac8af3da7ef985fb673ea3987a9afd62a
wangliwei <fqm_wlw@tftm.com.cn>
2021-01-06 14:11:20 +0800

    1

--- SPPSApi/Controllers/G12/FS1205Controller.cs
+++ SPPSApi/Controllers/G12/FS1205Controller.cs
@@ -103,5 +103,75 @@ namespace SPPSApi.Controllers.G12
         }
         #endregion
 
+        #region 更新到数据库
+        [HttpPost]
+        [EnableCors("any")]
+        public string TXTUpdateTableDetermine([FromBody] dynamic data)
+        {
+            string strToken = Request.Headers["X-Token"];
+            if (!isLogin(strToken))
+            {
+                return error_login();
+            }
+            LoginInfo loginInfo = getLoginByToken(strToken);
+            //以下开始业务处理
+            ApiResult apiResult = new ApiResult();
+            dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
+            string vcMonth = dataForm.vcMonth;
+            string vcWeek = dataForm.vcWeek;
+            string vcPlant = dataForm.vcPlant;
+            try
+            {
+                DataTable dt = fS1205_Logic.TXTSearchWeekLevelSchedule(vcMonth, vcWeek, vcPlant);
+                dt.Columns.Remove("iAutoId");
+                if (dt.Rows.Count > 0)
+                {
+                    string Month = dt.Rows[0]["vcMonth"].ToString();//获取数据源中的对象月
+                    string Week = dt.Rows[0]["vcWeek"].ToString();//获取数据源中的对象周
+                    string Plant = dt.Rows[0]["vcPlant"].ToString();//获取数据源中的工厂
+                    //检查数据正确性
+                    string msg = fS1205_Logic.TXTCheckDataSchedule(dt);
+                    if (msg.Length > 0)
+                    {
+                        apiResult.code = ComConstant.ERROR_CODE;
+                        apiResult.data = msg;
+                        return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+                    }
+                    else
+                    {
+                        dt.PrimaryKey = new DataColumn[]
+                        {
+                            dt.Columns["vcMonth"],
+                            dt.Columns["vcWeek"],
+                            dt.Columns["vcPlant"],
+                            dt.Columns["vcGC"],
+                            dt.Columns["vcZB"],
+                            dt.Columns["vcPartsno"]
+                        };
+                        fS1205_Logic.TXTUpdateTableSchedule(dt, Month, Week, Plant);
+                        //最终提示信息
+                        //更新成功重新获取数据源
+                        apiResult.code = ComConstant.SUCCESS_CODE;
+                        apiResult.data = "更新成功";
+                        return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+                    }
+                }
+                else
+                {
+                    apiResult.code = ComConstant.ERROR_CODE;
+                    apiResult.data = "无可更新的数据请先检索！";
+                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+                }
+            }
+            catch (Exception ex)
+            {
+                ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE0901", ex, loginInfo.UserId);
+                apiResult.code = ComConstant.ERROR_CODE;
+                apiResult.data = "检索失败";
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+        }
+        #endregion
+
     }
 }

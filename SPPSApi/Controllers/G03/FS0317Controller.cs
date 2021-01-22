0470dce31f6a0e23a2d5fcddd22ce17da0a67211
wangjiwei <794160766@qq.com>
2021-01-22 16:15:24 +0800

    1

--- SPPSApi/Controllers/G03/FS0317Controller.cs
+++ SPPSApi/Controllers/G03/FS0317Controller.cs
@@ -65,5 +65,58 @@ namespace SPPSApi.Controllers.G03
                 return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
             }
         }
+
+        #region 导出
+        [HttpPost]
+        [EnableCors("any")]
+        public string exportApi([FromBody] dynamic data)
+        {
+            string strToken = Request.Headers["X-Token"];
+            if (!isLogin(strToken))
+            {
+                return error_login();
+            }
+            LoginInfo loginInfo = getLoginByToken(strToken);
+            //以下开始业务处理
+            ApiResult apiResult = new ApiResult();
+            dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
+
+            JArray dateRangeArray = dataForm.dateRange;
+            List<string> dateRange = dateRangeArray.ToObject<List<string>>();
+
+            string supplierId = dataForm.supplierId == null ? "" : dataForm.supplierId;
+
+            try
+            {
+                string startTime = dateRange.Count > 0 ? dateRange[0].Replace("-", "") : "";
+                string endTime = dateRange.Count > 0 ? dateRange[1].Replace("-", "") : "";
+
+                DataTable dt = fs0317_logic.searchApi(startTime, endTime, supplierId);
+                string filepath = "";
+                string strMsg = "";
+
+                string[] heads = { "供应商代码", "展开点数", "超期点数", "超纳期百分比" };
+                string[] fields = { "vcSupplier_id", "total", "overNum", "per" };
+                filepath = ComFunction.DataTableToExcel(heads, fields, dt, _webHostEnvironment.ContentRootPath, loginInfo.UserId, FunctionID, ref strMsg);
+
+                if (filepath == "")
+                {
+                    apiResult.code = ComConstant.ERROR_CODE;
+                    apiResult.data = "导出生成文件失败";
+                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+                }
+                apiResult.code = ComConstant.SUCCESS_CODE;
+                apiResult.data = filepath;
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+            catch (Exception ex)
+            {
+                ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE1602", ex, loginInfo.UserId);
+                apiResult.code = ComConstant.ERROR_CODE;
+                apiResult.data = "导出失败";
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+        }
+        #endregion
     }
 }

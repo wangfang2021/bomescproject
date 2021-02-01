d834a91f6c897e9f8581dca755956404698b7a5d
wangjiwei <794160766@qq.com>
2021-02-01 09:49:30 +0800

    1

---
+++ Logic/FS0407_Logic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using Common;
+using DataAccess;
+
+namespace Logic
+{
+    public class FS0407_Logic
+    {
+        FS0407_DataAccess fs0407_dataAccess = new FS0407_DataAccess();
+
+        #region 检索
+
+        public DataTable SearchApi(string OrderNo, string strPartId)
+        {
+            return fs0407_dataAccess.searchApi(OrderNo, strPartId);
+        }
+        #endregion
+
+
+    }
+}

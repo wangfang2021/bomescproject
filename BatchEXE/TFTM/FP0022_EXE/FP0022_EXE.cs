5c97caf6a62f53b20ab4ff20ce0424efcff04bc5
wangliwei <fqm_wlw@tftm.com.cn>
2021-02-18 09:45:09 +0800

    1

--- BatchEXE/TFTM/FP0022_EXE/FP0022_EXE.cs
+++ BatchEXE/TFTM/FP0022_EXE/FP0022_EXE.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using BatchProcess;
+using Common;
+using System;
 
 namespace FP0022_EXE
 {
-    class FP0022_EXE
+    public class FP0022_EXE
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string PageId = "FP0022";
+            int iRet = Common.ComConstant.OK_CODE;
+            try
+            {
+                FP0022 pb = new FP0022();
+                if (!pb.main("000000"))
+                    iRet = Common.ComConstant.NG_CODE;
+                return;
+            }
+            catch (Exception ex)
+            {
+                iRet = Common.ComConstant.NG_CODE;
+                ComMessage.GetInstance().ProcessMessage(PageId, "M03PE2000", ex, "system");
+            }
+            finally
+            {
+                Environment.Exit(iRet);
+            }
         }
     }
 }

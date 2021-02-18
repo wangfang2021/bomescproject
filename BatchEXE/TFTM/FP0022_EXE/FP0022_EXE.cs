e6bac97900c239b5be8a0641a8600d07379aefaa
wangliwei <fqm_wlw@tftm.com.cn>
2021-02-18 09:42:29 +0800

    1

---
+++ BatchEXE/TFTM/FP0022_EXE/FP0022_EXE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FP0022_EXE
+{
+    class FP0022_EXE
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

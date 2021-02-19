534f3760b9379398f8d9589aa1cbb22f341e8c25
LJY <ljyi_tjqm@faw.com.cn>
2021-02-19 08:57:16 +0800

    ljy

--- Common/ComConnectionHelper.cs
+++ Common/ComConnectionHelper.cs
@@ -46,8 +46,11 @@ namespace Common
 
         #region 获取数据库连接字符串 
         public static string GetConnectionString()
-        {
-            return "Persist Security Info = False; User ID = sa; Password = Sa123; Initial Catalog = SPPSdb; Data Source =172.23.140.169";
+        { 
+            //服务器
+            //return "Persist Security Info = False; User ID = sa; Password = Sa123; Initial Catalog = SPPSdb; Data Source =172.23.140.169";
+            //本地
+            return "Persist Security Info = False; User ID = sa; Password = Server2017; Initial Catalog = SPPSdb; Data Source =.";
             //return "Persist Security Info = False; User ID = sa; Password = Server2008; Initial Catalog = SPPSdb001; Data Source =TJQM-FY\\SQLEXPRESS";
         }
         #endregion
@@ -59,6 +62,12 @@ namespace Common
         }
         #endregion
 
+        #region 获取数据库连接字符串_MAPS
+        public static string GetConnectionString_MAPS()
+        {
+            return "Persist Security Info = False; User ID = sa; Password = Server2017; Initial Catalog = MAPS; Data Source =.";
+        }
+        #endregion
 
         #region 创建数据连接,连接状态为Closed--FIFS
         public static SqlConnection CreateSqlConnection()
@@ -84,6 +93,23 @@ namespace Common
             {
                 SqlConnection conn_sql = new SqlConnection();
                 conn_sql.ConnectionString = GetConnectionString_NQC();
+                CloseConnection_SQL(ref conn_sql);
+                return conn_sql;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region 创建数据连接,连接状态为Closed--MAPS
+        public static SqlConnection CreateConnection_MAPS()
+        {
+            try
+            {
+                SqlConnection conn_sql = new SqlConnection();
+                conn_sql.ConnectionString = GetConnectionString_MAPS();
                 CloseConnection_SQL(ref conn_sql);
                 return conn_sql;
             }

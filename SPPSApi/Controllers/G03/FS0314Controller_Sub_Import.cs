9d3cfe4a12ca6dc0445f5df70e85256b2b6df130
wangjiwei <794160766@qq.com>
2021-02-25 15:21:55 +0800

    123

---
+++ SPPSApi/Controllers/G03/FS0314Controller_Sub_Import.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using Common;
+using Logic;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SPPSApi.Controllers.G03
+{
+    [Route("api/FS0314_Sub_Import/[action]")]
+    [EnableCors("any")]
+    [ApiController]
+    public class FS0314Controller_Sub_Import : BaseController
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        FS0314_Logic fs0314_Logic = new FS0314_Logic();
+        private readonly string FunctionID = "FS0314";
+
+        public FS0314Controller_Sub_Import(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        #region 导入之后点保存
+        [HttpPost]
+        [EnableCors("any")]
+        public string importSaveApi([FromBody]dynamic data)
+        {
+            //验证是否登录
+            string strToken = Request.Headers["X-Token"];
+            if (!isLogin(strToken))
+            {
+                return error_login();
+            }
+            LoginInfo loginInfo = getLoginByToken(strToken);
+            //以下开始业务处理
+            ApiResult apiResult = new ApiResult();
+            dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
+            JArray fileNameList = dataForm.fileNameList;
+            string hashCode = dataForm.hashCode;
+            string fileSavePath = _webHostEnvironment.ContentRootPath + Path.DirectorySeparatorChar + "Doc" + Path.DirectorySeparatorChar + "upload" + Path.DirectorySeparatorChar + hashCode + Path.DirectorySeparatorChar;
+            try
+            {
+                if (!Directory.Exists(fileSavePath))
+                {
+                    ComFunction.DeleteFolder(fileSavePath);//读取异常则，删除文件夹，全部重新上传
+                    apiResult.code = ComConstant.ERROR_CODE;
+                    apiResult.data = "没有要导入的文件，请重新上传！";
+                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+                }
+                DirectoryInfo theFolder = new DirectoryInfo(fileSavePath);
+                string strMsg = "";
+                string[,] headers = new string[,] {{"供应商代码","供应商名称","生产商名称","生产商地址","联系人1", "联系人1电话","联系人1邮箱","联系人2","联系人2电话","联系人2邮箱","联系人3","联系人3电话","联系人3邮箱"},
+                                                {"vcSupplier_id","vcSupplier_name","vcProduct_name","vcAddress","vcLXR1","vcPhone1","vcEmail1","vcLXR2","vcPhone2","vcEmail2","vcLXR3","vcPhone3","vcEmail3"},
+                                                {FieldCheck.NumChar,"","","","",FieldCheck.NumChar,"","",FieldCheck.NumChar,"","",FieldCheck.NumChar,""},
+                                                {"4","0","0","0","0","0", "0", "0", "0", "0", "0", "0", "0"},//最大长度设定,不校验最大长度用0
+                                                {"4","1","1","1","1","0", "1", "0", "0", "0", "0", "0", "0"}};//最小长度设定,可以为空用0
+
+
+
+                DataTable importDt = new DataTable();
+                foreach (FileInfo info in theFolder.GetFiles())
+                {
+                    DataTable dt = ComFunction.ExcelToDataTable(info.FullName, "sheet1", headers, ref strMsg);
+                    if (strMsg != "")
+                    {
+                        ComFunction.DeleteFolder(fileSavePath);//读取异常则，删除文件夹，全部重新上传
+                        apiResult.code = ComConstant.ERROR_CODE;
+                        apiResult.data = "导入终止，文件" + info.Name + ":" + strMsg;
+                        return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+                    }
+                    if (importDt.Columns.Count == 0)
+                        importDt = dt.Clone();
+                    if (dt.Rows.Count == 0)
+                    {
+                        ComFunction.DeleteFolder(fileSavePath);//读取异常则，删除文件夹，全部重新上传
+                        apiResult.code = ComConstant.ERROR_CODE;
+                        apiResult.data = "导入终止，文件" + info.Name + "没有要导入的数据";
+                        return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+                    }
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        importDt.ImportRow(row);
+                    }
+                }
+                ComFunction.DeleteFolder(fileSavePath);//读取数据后删除文件夹
+
+
+
+
+                fs0314_Logic.importSave(importDt, loginInfo.UserId);
+                apiResult.code = ComConstant.SUCCESS_CODE;
+                apiResult.data = "保存成功";
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+            catch (Exception ex)
+            {
+                ComFunction.DeleteFolder(fileSavePath);//读取异常则，删除文件夹，全部重新上传
+                ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE0605", ex, loginInfo.UserId);
+                apiResult.code = ComConstant.ERROR_CODE;
+                apiResult.data = "保存失败" + ex.Message;
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+        }
+        #endregion
+
+
+
+    }
+}

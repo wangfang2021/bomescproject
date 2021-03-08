033a63adb7ea56f0faa62ac74700e218e05e2a8a
zhanghaipeng <arale22@163.com>
2021-03-08 23:10:30 +0800

    123

--- SPPSApi/Controllers/G13/FS1306Controller.cs
+++ SPPSApi/Controllers/G13/FS1306Controller.cs
@@ -20,9 +20,9 @@ using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace SPPSApi.Controllers.G05
+namespace SPPSApi.Controllers.G13
 {
-    [Route("api/FS0504/[action]")]
+    [Route("api/FS1306/[action]")]
     [EnableCors("any")]
     [ApiController]
     public class FS1306Controller : BaseController
@@ -96,73 +96,6 @@ namespace SPPSApi.Controllers.G05
                 ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE0901", ex, loginInfo.UserId);
                 apiResult.code = ComConstant.ERROR_CODE;
                 apiResult.data = "检索失败";
-                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-            }
-        }
-        /// <summary>
-        /// 下载方法
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [EnableCors("any")]
-        public string exportApi([FromBody] dynamic data)
-        {
-            string strToken = Request.Headers["X-Token"];
-            if (!isLogin(strToken))
-            {
-                return error_login();
-            }
-            LoginInfo loginInfo = getLoginByToken(strToken);
-            //以下开始业务处理
-            ApiResult apiResult = new ApiResult();
-            dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
-
-            string strDyState = dataForm.DyState;
-            string strHyState = dataForm.HyState;
-            string strPartId = dataForm.PartId;
-            string strCarModel = dataForm.CarModel;
-            string strInOut = dataForm.InOut;
-            string strOrderingMethod = dataForm.OrderingMethod;
-            string strOrderPlant = dataForm.OrderPlant;
-            string strHaoJiu = dataForm.HaoJiu;
-            string strSupplierId = dataForm.SupplierId;
-            string strSupplierPlant = dataForm.SupplierPlant;
-            string strDataState = dataForm.DataState;
-            try
-            {
-                string strYearMonth = dataForm.YearMonth == null ? "" : Convert.ToDateTime(dataForm.YearMonth).ToString("yyyyMM");
-                string strYearMonth_2 = dataForm.YearMonth == null ? "" : Convert.ToDateTime(dataForm.YearMonth).AddMonths(1).ToString("yyyyMM");
-                string strYearMonth_3 = dataForm.YearMonth == null ? "" : Convert.ToDateTime(dataForm.YearMonth).AddMonths(2).ToString("yyyyMM");
-                DataTable dataTable = fs0602_Logic.getSearchInfo(strYearMonth, strDyState, strHyState, strPartId, strCarModel,
-                     strInOut, strOrderingMethod, strOrderPlant, strHaoJiu, strSupplierId, strSupplierPlant, strDataState);
-
-                string[] fields = {"vcYearMonth","vcDyState_Name","vcHyState_Name","vcPart_id","vcCarfamilyCode","vcHaoJiu","vcOrderingMethod",
-                    "vcOrderPlant","vcInOut",
-                    "vcSupplierId","vcSupplierPlant","iPackingQty",
-                    "iCbSOQN","decCbBdl","iCbSOQN1","iCbSOQN2",
-                    "iTzhSOQN","iTzhSOQN1","iTzhSOQN2",
-                    "iHySOQN","iHySOQN1","iHySOQN2",
-                    "dExpectTime","dSReplyTime","vcOverDue","dHyTime"
-                };
-
-                //string filepath = ComFunction.generateExcelWithXlt(dataTable, fields, _webHostEnvironment.ContentRootPath, "FS0602_Export.xlsx", 1, loginInfo.UserId, FunctionID);
-                string filepath = fs0602_Logic.generateExcelWithXlt(dataTable, fields, _webHostEnvironment.ContentRootPath, "FS0602_Export.xlsx", 2, loginInfo.UserId, FunctionID, strYearMonth, strYearMonth_2, strYearMonth_3);
-                if (filepath == "")
-                {
-                    apiResult.code = ComConstant.ERROR_CODE;
-                    apiResult.data = "导出生成文件失败";
-                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-                }
-                apiResult.code = ComConstant.SUCCESS_CODE;
-                apiResult.data = filepath;
-                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-            }
-            catch (Exception ex)
-            {
-                ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE0904", ex, loginInfo.UserId);
-                apiResult.code = ComConstant.ERROR_CODE;
-                apiResult.data = "导出失败";
                 return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
             }
         }

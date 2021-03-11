acae7cd098d08e29282645042b4148df5af68f5c
wangliwei <fqm_wlw@tftm.com.cn>
2021-03-11 19:01:39 +0800

    1

--- DataAccess/FS0626_DataAccess.cs
+++ DataAccess/FS0626_DataAccess.cs
@@ -352,7 +352,7 @@ namespace DataAccess
                     cmdUpdate2.CommandText += "update TOutsidePurchaseManage ";
                     cmdUpdate2.CommandText += "set TOutsidePurchaseManage.vcInjectionFactory=d.vcValue5 ";
                     cmdUpdate2.CommandText += "from (select vcValue1,vcValue2,vcValue5 from TOutCode ";
-                    cmdUpdate2.CommandText += "      where vcCodeId='C010' and convert(date,vcValue3)<=GETDATE() and convert(date,vcValue4)>=GETDATE()) d ";
+                    cmdUpdate2.CommandText += "      where vcCodeId='C010' and convert(date,vcValue3)<=GETDATE() and convert(date,vcValue4)>=GETDATE() and vcIsColum='0') d ";
                     cmdUpdate2.CommandText += "where TOutsidePurchaseManage.vcSupplier_id=d.vcValue1 and TOutsidePurchaseManage.vcWorkArea=d.vcValue2 ";
                     cmdUpdate2.CommandText += "and TOutsidePurchaseManage.vcTargetMonth>='" + vcDateFrom + "' and TOutsidePurchaseManage.vcTargetMonth<='" + vcDateTo + "' ";
                     cmdUpdate2.Transaction = trans;
@@ -416,7 +416,7 @@ namespace DataAccess
                     cmdUpdate2.CommandText += "update TOutsidePurchaseManage ";
                     cmdUpdate2.CommandText += "set TOutsidePurchaseManage.vcInjectionFactory=d.vcValue5 ";
                     cmdUpdate2.CommandText += "from (select vcValue1,vcValue2,vcValue5 from TOutCode ";
-                    cmdUpdate2.CommandText += "      where vcCodeId='C010' and convert(date,vcValue3)<=GETDATE() and convert(date,vcValue4)>=GETDATE()) d ";
+                    cmdUpdate2.CommandText += "      where vcCodeId='C010' and convert(date,vcValue3)<=GETDATE() and convert(date,vcValue4)>=GETDATE() and vcIsColum='0') d ";
                     cmdUpdate2.CommandText += "where TOutsidePurchaseManage.vcSupplier_id=d.vcValue1 and TOutsidePurchaseManage.vcWorkArea=d.vcValue2 ";
                     cmdUpdate2.CommandText += "and TOutsidePurchaseManage.vcTargetMonth='" + vcTargetMonth + "' ";
                     cmdUpdate2.Transaction = trans;

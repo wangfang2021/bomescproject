d90176fa8d1d0242f5db3753d020e65b9de137e8
wangjiwei <794160766@qq.com>
2021-03-11 11:16:25 +0800

    1

---
+++ TFTM/FP0021_EXE/FP0021_EXE.cs
@@ -0,0 +1,33 @@
+﻿using BatchProcess;
+using Common;
+using System;
+
+
+namespace FP0020_EXE
+{
+    class FP0020_EXE
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string PageId = "FP0021";
+            int iRet = Common.ComConstant.OK_CODE;
+            try
+            {
+                FP0021 pb = new FP0021();
+                if (!pb.main("000000"))
+                    iRet = Common.ComConstant.NG_CODE;
+                return;
+            }
+            catch (Exception ex)
+            {
+                iRet = Common.ComConstant.NG_CODE;
+                ComMessage.GetInstance().ProcessMessage(PageId, "M03PE0200", ex, "system");
+            }
+            finally
+            {
+                Environment.Exit(iRet);
+            }
+        }
+    }
+}

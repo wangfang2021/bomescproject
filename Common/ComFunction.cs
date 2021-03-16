25cf445a5d74a6ff8f6e3a2415ed8a13f5f97872
wangliwei <fqm_wlw@tftm.com.cn>
2021-03-16 08:22:57 +0800

    1

--- Common/ComFunction.cs
+++ Common/ComFunction.cs
@@ -665,7 +665,7 @@ namespace Common
                 }
 
                 if (sheet != null)
-                { 
+                {
                     IRow firstRow = sheet.GetRow(0);
                     int cellCount = firstRow.LastCellNum; //一行最后一个cell的编号 即总的列数
 

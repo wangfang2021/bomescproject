fd43c9c591d0ca78eec2059c53262b8db3a8c9a8
fang wang <15822440710@163.com>
2021-03-18 12:49:09 +0800

    666

--- SPPSApi/Controllers/G17/FS1702Controller_Sub_jinji.cs
+++ SPPSApi/Controllers/G17/FS1702Controller_Sub_jinji.cs
@@ -69,6 +69,48 @@ namespace SPPSApi.Controllers.G17
         }
         #endregion
 
+        #region 导出
+        [HttpPost]
+        [EnableCors("any")]
+        public string exportApi([FromBody] dynamic data)
+        {
+            string strToken = Request.Headers["X-Token"];
+            if (!isLogin(strToken))
+            {
+                return error_login();
+            }
+            LoginInfo loginInfo = getLoginByToken(strToken);
+            //以下开始业务处理
+            ApiResult apiResult = new ApiResult();
+            dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
+            string vcPart_id = dataForm.vcPart_id;
+            try
+            {
+                DataTable dt = fs1702_Logic.Search_jinji(vcPart_id);
+                string[] heads = { "品番", "数量", "原因" };
+                string[] fields = { "vcPart_id", "iQuantity", "iQuantity" };
+                string strMsg = "";
+                string filepath = ComFunction.DataTableToExcel(heads, fields, dt, _webHostEnvironment.ContentRootPath, loginInfo.UserId, FunctionID, ref strMsg);
+                if (strMsg != "")
+                {
+                    apiResult.code = ComConstant.ERROR_CODE;
+                    apiResult.data = strMsg;
+                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+                }
+                apiResult.code = ComConstant.SUCCESS_CODE;
+                apiResult.data = filepath;
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+            catch (Exception ex)
+            {
+                ComMessage.GetInstance().ProcessMessage(FunctionID, "M08UE1006", ex, loginInfo.UserId);
+                apiResult.code = ComConstant.ERROR_CODE;
+                apiResult.data = "导出品目关系失败";
+                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
+            }
+        }
+        #endregion
+
         #region 保存
         [HttpPost]
         [EnableCors("any")]
@@ -167,7 +209,7 @@ namespace SPPSApi.Controllers.G17
                     apiResult.data = "最少选择一条数据！";
                     return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
                 }
-                //fs1702_Logic.Del_pm(listInfoData, loginInfo.UserId);
+                fs1702_Logic.Del_jinji(listInfoData, loginInfo.UserId);
                 apiResult.code = ComConstant.SUCCESS_CODE;
                 apiResult.data = null;
                 return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);

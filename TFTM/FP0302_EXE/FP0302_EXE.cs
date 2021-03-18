6e4c0669b33a7301afbc7a78a8fb7aa0fc15550e
wangjiwei <794160766@qq.com>
2021-03-18 09:29:11 +0800

    1

--- TFTM/FP0302_EXE/FP0302_EXE.cs
+++ TFTM/FP0302_EXE/FP0302_EXE.cs
@@ -1,5 +1,5 @@
-﻿using BatchProcess;
-using Common;
+﻿
+
 using System;
 
 namespace FP0302_EXE
@@ -9,26 +9,7 @@ namespace FP0302_EXE
         [STAThread]
         static void Main(string[] args)
         {
-            string PageId = "FP0302";
-            int iRet = Common.ComConstant.OK_CODE;
-            try
-            {
-                FP0302 pb = new FP0302();
-                if (!pb.main("000000", 0))
-                    iRet = Common.ComConstant.NG_CODE;
-                if (!pb.main("000000", 1))
-                    iRet = Common.ComConstant.NG_CODE;
-                return;
-            }
-            catch (Exception ex)
-            {
-                iRet = Common.ComConstant.NG_CODE;
-                ComMessage.GetInstance().ProcessMessage(PageId, "M03PE0200", ex, "system");
-            }
-            finally
-            {
-                Environment.Exit(iRet);
-            }
+
         }
     }
 }

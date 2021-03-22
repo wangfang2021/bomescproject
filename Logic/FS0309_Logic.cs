7b28985e91f02c82ba635a2ffe6e90ac8db1b988
hujun <uviwwer@163.com>
2021-03-22 11:11:58 +0800

    1

--- Logic/FS0309_Logic.cs
+++ Logic/FS0309_Logic.cs
@@ -23,12 +23,12 @@ namespace Logic
 
 
         #region 按检索条件检索,返回dt,注意这个dt返回的时候convert了
-        public DataTable Search(string strMaxNum,string strChange, string strPart_id, string strOriginCompany, string strHaoJiu
+        public DataTable Search(string strChange, string strPart_id, string strOriginCompany, string strHaoJiu
             , string strProjectType, string strPriceChangeInfo, string strCarTypeDev, string strSupplier_id
             , string strReceiver, string strPriceState
             )
         {
-            return convert(fs0309_DataAccess.Search(strMaxNum,strChange, strPart_id, strOriginCompany, strHaoJiu
+            return convert(fs0309_DataAccess.Search(strChange, strPart_id, strOriginCompany, strHaoJiu
             , strProjectType, strPriceChangeInfo, strCarTypeDev, strSupplier_id
             , strReceiver, strPriceState
             ));

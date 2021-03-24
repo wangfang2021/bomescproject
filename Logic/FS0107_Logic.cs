3413df02222c278f11d27f3237f60d9155806c94
964587145@qq.com <964587145@qq.com>
2021-03-24 16:52:18 +0800

    commit

---
+++ Logic/FS0107_Logic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataAccess;
+using System.Collections;
+
+
+namespace Logic
+{
+    public class FS0107_Logic
+    {
+        FS0107_DataAccess fs0107_DataAccess;
+
+        public FS0107_Logic()
+        {
+            fs0107_DataAccess = new FS0107_DataAccess();
+
+        }
+
+        public DataTable BindConst()
+        {
+            return fs0107_DataAccess.BindConst();
+        }
+
+        public DataTable Search(string typeCode, string vcValue1, string vcValue2)
+        {
+            return fs0107_DataAccess.Search(typeCode, vcValue1, vcValue2);
+        }
+   
+        public void Save(List<Dictionary<string, object>> listInfoData, string userId, ref string strErrorPartId)
+        {
+            fs0107_DataAccess.Save(listInfoData, userId, ref strErrorPartId);
+        }
+
+        public void Del(List<Dictionary<string, object>> listInfoData, string userId)
+        {
+            fs0107_DataAccess.Del(listInfoData, userId);
+        }
+
+        public DataTable CheckDistinctByTable(DataTable dtadd)
+        {
+            return fs0107_DataAccess.CheckDistinctByTable(dtadd);
+        }
+    }
+}

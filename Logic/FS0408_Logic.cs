82757fd5ef18094b612d9a97cfff9ab8d5c68667
wangjiwei <794160766@qq.com>
2021-03-26 17:03:54 +0800

    1

--- Logic/FS0408_Logic.cs
+++ Logic/FS0408_Logic.cs
@@ -47,7 +47,7 @@ namespace Logic
         }
 
         #region 发送
-        public void sendMail(List<Dictionary<string, Object>> listInfoData, string strUserId, string strUserName, ref string refMsg, string Email, string unit, string unitCode)
+        public void sendMail(List<Dictionary<string, Object>> listInfoData, string strUserId, string strUserName, ref string refMsg, string Email, string unit)
         {
             try
             {
@@ -95,11 +95,14 @@ namespace Logic
         #endregion
 
         #region 承认
-        public void commitMail(List<Dictionary<string, Object>> listInfoData, string strUserId, string strUserName, ref string refMsg, string Email, string unit)
+        public void commitMail(List<Dictionary<string, Object>> listInfoData, string strUserId, string strUserName, ref string refMsg, string Email, string unit, string unitCode)
         {
             try
             {
                 fs0408_dataAccess.changeState(listInfoData[0]["iAuto_id"].ToString(), "2", strUserId, "1");
+                string range = listInfoData[0]["vcRange"].ToString();
+                string total = fs0408_dataAccess.getTotal(listInfoData[0]["vcRelation"].ToString());
+
                 //TODO 发送邮件
                 string strSubject = "FTMS结果反馈。";
                 DataTable cCDt = null;
@@ -118,7 +121,8 @@ namespace Logic
                 StringBuilder sbr = new StringBuilder();
                 sbr.AppendLine("<p class=\"ql-align-justify\"><strong style=\"color: black;\">TFTM &nbsp; 相关各位殿</strong></p>");
                 sbr.AppendLine("<p class=\"ql-align-justify\"><span style=\"color: black;\">FTMS财务&nbsp;" + strUserName + "</span></p><p class=\"ql-align-justify\"><strong>&nbsp;</strong></p>");
-                sbr.AppendLine("<p><strong style=\"color: black;\">金额一致</strong></p>");
+                //sbr.AppendLine("<p><strong style=\"color: black;\">金额一致</strong></p>");
+                sbr.AppendLine("<p><strong style=\"color: black;\">FTMS " + range + "&nbsp;对帐数据&nbsp;</strong><strong style=\"color: blue;\">&nbsp;<u>月度对帐数</u></strong><strong style=\"color: rgb(0, 51, 204);\"><u>据</u></strong><strong style=\"color: black;\"><u>&nbsp;" + total + "</u></strong><strong style=\"color: blue;\"><u>元&nbsp;&nbsp;</u></strong><strong><u>，</u></strong><strong style=\"color: black;\"><u>确认无误。</u></strong><strong><u>。</u></strong></p><p class=\"ql-align-justify\"><strong>&nbsp;</strong></p>");
                 sbr.AppendLine("<p class=\"ql-align-justify\"><strong>以</strong><strong style=\"color: black;\">上</strong></p><p><br></p>");
                 string EmailBody = sbr.ToString();
 

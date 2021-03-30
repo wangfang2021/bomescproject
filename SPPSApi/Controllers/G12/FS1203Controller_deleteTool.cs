44a077991ed3aa02c1b219171da6f05e3fd425dc
wangliwei <fqm_wlw@tftm.com.cn>
2021-03-30 17:04:39 +0800

    11

--- SPPSApi/Controllers/G12/FS1203Controller_deleteTool.cs
+++ SPPSApi/Controllers/G12/FS1203Controller_deleteTool.cs
@@ -36,42 +36,6 @@ namespace SPPSApi.Controllers.G12
             _webHostEnvironment = webHostEnvironment;
         }
 
-        #region 检索
-        [HttpPost]
-        [EnableCors("any")]
-        public string searchApi([FromBody] dynamic data)
-        {
-            string strToken = Request.Headers["X-Token"];
-            if (!isLogin(strToken))
-            {
-                return error_login();
-            }
-            LoginInfo loginInfo = getLoginByToken(strToken);
-            //以下开始业务处理
-            ApiResult apiResult = new ApiResult();
-            dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
-            string strMon = dataForm.vcMon;
-            try
-            {
-                DataTable dt = logic.getCutPlan(strMon);
-                DtConverter dtConverter = new DtConverter();
-                dtConverter.addField("vcModFlag", ConvertFieldType.BoolType, null);
-                dtConverter.addField("vcAddFlag", ConvertFieldType.BoolType, null);
-                List<Object> dataList = ComFunction.convertAllToResultByConverter(dt, dtConverter);
-                apiResult.code = ComConstant.SUCCESS_CODE;
-                apiResult.data = dataList;
-                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-            }
-            catch (Exception ex)
-            {
-                ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE0901", ex, loginInfo.UserId);
-                apiResult.code = ComConstant.ERROR_CODE;
-                apiResult.data = "检索失败";
-                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-            }
-        }
-        #endregion
-
         #region 删除
         [HttpPost]
         [EnableCors("any")]
@@ -89,8 +53,8 @@ namespace SPPSApi.Controllers.G12
             try
             {
                 dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
-                JArray checkedInfo = dataForm.multipleSelection;
-                List<Dictionary<string, Object>> listInfoData = checkedInfo.ToObject<List<Dictionary<string, Object>>>();
+                JArray lists = dataForm.temp.list;
+                List<Dictionary<string, object>> listInfoData = lists.ToObject<List<Dictionary<string, object>>>();
                 if (listInfoData.Count == 0)
                 {
                     apiResult.code = ComConstant.ERROR_CODE;
@@ -106,143 +70,9 @@ namespace SPPSApi.Controllers.G12
             {
                 ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE0909", ex, loginInfo.UserId);
                 apiResult.code = ComConstant.ERROR_CODE;
-                apiResult.data = "删除公式失败";
+                apiResult.data = "删除失败";
                 return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
             }
-        }
-        #endregion
-
-        #region 保存
-        [HttpPost]
-        [EnableCors("any")]
-        public string saveApi([FromBody] dynamic data)
-        {
-            //验证是否登录
-            string strToken = Request.Headers["X-Token"];
-            if (!isLogin(strToken))
-            {
-                return error_login();
-            }
-            LoginInfo loginInfo = getLoginByToken(strToken);
-            //以下开始业务处理
-            ApiResult apiResult = new ApiResult();
-            try
-            {
-                dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
-                JArray listInfo = dataForm.multipleSelection;
-                List<Dictionary<string, Object>> listInfoData = listInfo.ToObject<List<Dictionary<string, Object>>>();
-                bool hasFind = false;//是否找到需要新增或者修改的数据
-                for (int i = 0; i < listInfoData.Count; i++)
-                {
-                    bool bModFlag = (bool)listInfoData[i]["vcModFlag"];//true可编辑,false不可编辑
-                    bool bAddFlag = (bool)listInfoData[i]["vcAddFlag"];//true可编辑,false不可编辑
-                    hasFind = true;
-                }
-                if (!hasFind)
-                {
-                    apiResult.code = ComConstant.ERROR_CODE;
-                    apiResult.data = "最少有一个编辑行！";
-                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-                }
-                //开始数据验证
-                if (hasFind)
-                {
-                    string[,] strField = new string[,] {{"对象月","品番","受入","看板订单号","连番","紧急区分"},
-                                                {"vcMonth","vcPartsno","vcDock","vcKBorderno","vcKBSerial","vcEDflag"},
-                                                {"","","","","",""},
-                                                {"7","12","2","18","4","2"},//最大长度设定,不校验最大长度用0
-                                                {"7","12","2","12","4","0"},//最小长度设定,可以为空用0
-                                                {"1","2","3","4","5","6"}//前台显示列号，从0开始计算,注意有选择框的是0
-                    };
-                }
-  
-                DataTable tb = ListToDataTable(listInfoData);
-                tb.Columns.RemoveAt(0);
-                string strMsg = "";
-                strMsg = logic.UpdateCutPlanTMP(tb, loginInfo.UserId);
-                if (strMsg != "")
-                {
-                    apiResult.code = ComConstant.ERROR_CODE;
-                    apiResult.data = strMsg;
-                    apiResult.flag = Convert.ToInt32(ERROR_FLAG.弹窗提示);
-                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-                }
-                apiResult.code = ComConstant.SUCCESS_CODE;
-                apiResult.data = strMsg;
-                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-            }
-            catch (Exception ex)
-            {
-                ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE0908", ex, loginInfo.UserId);
-                apiResult.code = ComConstant.ERROR_CODE;
-                apiResult.data = "保存失败";
-                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-            }
-        }
-        #endregion
-
-        #region 更新到计划
-        [HttpPost]
-        [EnableCors("any")]
-        public string updatePlan([FromBody] dynamic data)
-        {
-            string strToken = Request.Headers["X-Token"];
-            if (!isLogin(strToken))
-            {
-                return error_login();
-            }
-            LoginInfo loginInfo = getLoginByToken(strToken);
-            //以下开始业务处理
-            ApiResult apiResult = new ApiResult();
-            dynamic dataForm = JsonConvert.DeserializeObject(Convert.ToString(data));
-            string strMon = dataForm.vcMon;
-            try
-            {
-                string strMsg = "";
-                strMsg = logic.UpdatePlan(strMon, loginInfo.UserId);
-                if (strMsg != "")
-                {
-                    apiResult.code = ComConstant.ERROR_CODE;
-                    apiResult.data = strMsg;
-                    apiResult.flag = Convert.ToInt32(ERROR_FLAG.弹窗提示);
-                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-                }
-                apiResult.code = ComConstant.SUCCESS_CODE;
-                apiResult.data = "计划削减成功！";
-                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-            }
-            catch (Exception ex)
-            {
-                ComMessage.GetInstance().ProcessMessage(FunctionID, "M03UE0901", ex, loginInfo.UserId);
-                apiResult.code = ComConstant.ERROR_CODE;
-                apiResult.data = "计划削减失败！";
-                return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-            }
-        }
-        #endregion
-
-        #region 通用方法
-        public DataTable ListToDataTable(List<Dictionary<string, Object>> listInfoData)
-        {
-            DataTable tb = new DataTable();
-            if (listInfoData.Count > 0)
-            {
-                Dictionary<string, object> li = listInfoData[0];
-                for (int i = 0; i < li.Count; i++)
-                {
-                    string colName = li.ToList()[i].Key;
-                    string colType = li.ToList()[i].Value.GetType().Name;
-                    tb.Columns.Add(new DataColumn(colName, li.ToList()[i].Value.GetType()));
-                }
-                foreach (Dictionary<string, object> li1 in listInfoData)
-                {
-                    DataRow r = tb.NewRow();
-                    for (int j = 0; j < tb.Columns.Count; j++)
-                        r[j] = li1[tb.Columns[j].ColumnName].ToString();
-                    tb.Rows.Add(r);
-                }
-            }
-            return tb;
         }
         #endregion
     }

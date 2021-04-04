57ac5085610a946c84f638d763ea0e3301182031
wangliwei <fqm_wlw@tftm.com.cn>
2021-04-04 12:07:46 +0800

    1

--- SPPSApi/Controllers/G12/FS1211Controller.cs
+++ SPPSApi/Controllers/G12/FS1211Controller.cs
@@ -119,6 +119,7 @@ namespace SPPSApi.Controllers.G12
                 }
                 DtConverter dtConverter = new DtConverter();
                 List<Object> dataList = ComFunction.convertAllToResultByConverter(dt, dtConverter);
+                dtConverter.addField("dStart", ConvertFieldType.DateType, "yyyy/MM/dd HH:mm:ss");
                 apiResult.code = ComConstant.SUCCESS_CODE;
                 apiResult.data = dataList;
                 return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);

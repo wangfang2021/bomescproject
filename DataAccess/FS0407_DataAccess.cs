ddc7209a295fcf9acb66f24781bdbae7814f591d
wangjiwei <794160766@qq.com>
2021-04-07 17:09:34 +0800

    1

--- DataAccess/FS0407_DataAccess.cs
+++ DataAccess/FS0407_DataAccess.cs
@@ -19,7 +19,7 @@ namespace DataAccess
                 sbr.AppendLine("SELECT T2.iAutoId as LinId,T7.vcName as vcState_name,T1.vcState,T1.vcOrderNo,T1.vcPart_id,T5.vcName as vcOrderPlant,");
                 sbr.AppendLine("t3.vcName as vcInOut,T4.vcName as vcHaoJiu,T6.vcName as vcOESP,T1.vcSupplierId,T1.vcSupplierPlant,");
                 sbr.AppendLine("t1.vcSupplierPlace,T1.vcSufferIn,t1.iPackingQty,t1.iOrderQuantity,");
-                sbr.AppendLine("t2.iDuiYingQuantity,t2.dDeliveryDate,t2.dOutPutDate,t1.dReplyOverDate,");
+                sbr.AppendLine("t2.iDuiYingQuantity,t2.dDeliveryDate,t2.dOutPutDate,CONVERT(VARCHAR(10),t2.dOutPutDate,111) as dOutPutDateDC,t1.dReplyOverDate,");
                 sbr.AppendLine("'0' as bModFlag,'0' as bAddFlag,'' as vcBgColor,");
                 sbr.AppendLine("CASE WHEN T1.vcDelete='1' THEN '0' ELSE (CASE WHEN T1.vcState='3' THEN '0' ELSE '1' END)  END bSelectFlag FROM ");
                 sbr.AppendLine("(SELECT [iAutoId] as LinId,[vcStatus] as vcState,[vcOrderNo] as vcOrderNo,[vcPart_id] as vcPart_id,[vcOrderPlant] as vcOrderPlant,");

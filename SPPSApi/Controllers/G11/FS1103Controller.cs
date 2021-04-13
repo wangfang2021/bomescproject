6b4b485dbb939387743f7182ad528fd26817a741
zhanghaipeng <arale22@163.com>
2021-04-13 14:54:22 +0800

    123

--- SPPSApi/Controllers/G11/FS1103Controller.cs
+++ SPPSApi/Controllers/G11/FS1103Controller.cs
@@ -151,62 +151,80 @@ namespace SPPSApi.Controllers.G11
                         return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
                     }
                     #region 调用webApi打印
-                    string strPrinterName = fS0603_Logic.getPrinterName("FS1103", loginInfo.UserId);
-                    //创建 HTTP 绑定对象
                     string file_crv = _webHostEnvironment.ContentRootPath + Path.DirectorySeparatorChar + "Doc" + Path.DirectorySeparatorChar + "CryReports" + Path.DirectorySeparatorChar;
                     if (fS1103_Logic.getTempInfo(loginInfo.UserId, "input").Rows.Count != 0)
                     {
-                        var binding = new BasicHttpBinding();
-                        //根据 WebService 的 URL 构建终端点对象
-                        var endpoint = new EndpointAddress(@"http://172.23.238.179/WebAPI/WebServiceAPI.asmx");
-                        //创建调用接口的工厂，注意这里泛型只能传入接口
-                        var factory = new ChannelFactory<WebServiceAPISoap>(binding, endpoint);
-                        //从工厂获取具体的调用实例
-                        var callClient = factory.CreateChannel();
-                        setCRVPrintRequestBody Body = new setCRVPrintRequestBody();
-                        Body.strCRVName = file_crv + "crv_FS1103_input.rpt";
-                        Body.strScrpit = "SELECT * from tPrintTemp_input_FS1103 where vcOperatorID='" + loginInfo.UserId + "' order by vcInno";
-                        Body.strPrinterName = strPrinterName;
-                        Body.sqlUserID = "sa";
-                        Body.sqlPassword = "SPPS_Server2019";
-                        Body.sqlCatalog = "SPPSdb";
-                        Body.sqlSource = "172.23.180.116";
-                        //调用具体的方法，这里是 HelloWorldAsync 方法
-                        Task<setCRVPrintResponse> responseTask = callClient.setCRVPrintAsync(new setCRVPrintRequest(Body));
-                        //获取结果
-                        setCRVPrintResponse response = responseTask.Result;
-                        if (response.Body.setCRVPrintResult != "打印成功")
+                        DataTable dtPrinterInfo = fS0603_Logic.getPrinterInfo("指令书", loginInfo.UserId);
+                        if (dtPrinterInfo.Rows.Count != 0)
+                        {
+                            var binding = new BasicHttpBinding();
+                            //根据 WebService 的 URL 构建终端点对象
+                            var endpoint = new EndpointAddress(dtPrinterInfo.Rows[0]["vcWebAPI"].ToString());
+                            //创建调用接口的工厂，注意这里泛型只能传入接口
+                            var factory = new ChannelFactory<WebServiceAPISoap>(binding, endpoint);
+                            //从工厂获取具体的调用实例
+                            var callClient = factory.CreateChannel();
+                            setCRVPrintRequestBody Body = new setCRVPrintRequestBody();
+                            Body.strScrpit = "SELECT * from tPrintTemp_input_FS1103 where vcOperatorID='" + loginInfo.UserId + "' order by vcInno";
+                            Body.strCRVName = file_crv + dtPrinterInfo.Rows[0]["vcReports"].ToString();
+                            Body.strPrinterName = dtPrinterInfo.Rows[0]["vcPrinter"].ToString();
+                            Body.sqlUserID = dtPrinterInfo.Rows[0]["vcSqlUserID"].ToString();
+                            Body.sqlPassword = dtPrinterInfo.Rows[0]["vcSqlPassword"].ToString();
+                            Body.sqlCatalog = dtPrinterInfo.Rows[0]["vcSqlCatalog"].ToString();
+                            Body.sqlSource = dtPrinterInfo.Rows[0]["vcSqlSource"].ToString();
+                            //调用具体的方法，这里是 HelloWorldAsync 方法
+                            Task<setCRVPrintResponse> responseTask = callClient.setCRVPrintAsync(new setCRVPrintRequest(Body));
+                            //获取结果
+                            setCRVPrintResponse response = responseTask.Result;
+                            if (response.Body.setCRVPrintResult != "打印成功")
+                            {
+                                DataRow dataRow = dtMessage.NewRow();
+                                dataRow["vcMessage"] = "入库指令书打印失败，请联系管理员进行打印接口故障检查。";
+                                dtMessage.Rows.Add(dataRow);
+                            }
+                        }
+                        else
                         {
                             DataRow dataRow = dtMessage.NewRow();
-                            dataRow["vcMessage"] = "入库指令书打印失败，请联系管理员进行打印接口故障检查。";
+                            dataRow["vcMessage"] = "没有指令书打印机信息，请联系管理员维护。";
                             dtMessage.Rows.Add(dataRow);
                         }
                     }
                     if (fS1103_Logic.getTempInfo(loginInfo.UserId, "tag").Rows.Count != 0)
                     {
-                        var binding = new BasicHttpBinding();
-                        //根据 WebService 的 URL 构建终端点对象
-                        var endpoint = new EndpointAddress(@"http://172.23.238.179/WebAPI/WebServiceAPI.asmx");
-                        //创建调用接口的工厂，注意这里泛型只能传入接口
-                        var factory = new ChannelFactory<WebServiceAPISoap>(binding, endpoint);
-                        //从工厂获取具体的调用实例
-                        var callClient = factory.CreateChannel();
-                        setCRVPrintRequestBody Body = new setCRVPrintRequestBody();
-                        Body.strCRVName = file_crv + "crv_FS1103_tag_temp.rpt";
-                        Body.strScrpit = "SELECT * from tPrintTemp_tag_FS1103 where vcOperatorID='" + loginInfo.UserId + "' order by vcInno,vcPrintcount";
-                        Body.strPrinterName = strPrinterName;
-                        Body.sqlUserID = "sa";
-                        Body.sqlPassword = "SPPS_Server2019";
-                        Body.sqlCatalog = "SPPSdb";
-                        Body.sqlSource = "172.23.180.116";
-                        //调用具体的方法，这里是 HelloWorldAsync 方法
-                        Task<setCRVPrintResponse> responseTask = callClient.setCRVPrintAsync(new setCRVPrintRequest(Body));
-                        //获取结果
-                        setCRVPrintResponse response = responseTask.Result;
-                        if (response.Body.setCRVPrintResult != "打印成功")
+                        DataTable dtPrinterInfo = fS0603_Logic.getPrinterInfo("标签", loginInfo.UserId);
+                        if (dtPrinterInfo.Rows.Count != 0)
+                        {
+                            var binding = new BasicHttpBinding();
+                            //根据 WebService 的 URL 构建终端点对象
+                            var endpoint = new EndpointAddress(dtPrinterInfo.Rows[0]["vcWebAPI"].ToString());
+                            //创建调用接口的工厂，注意这里泛型只能传入接口
+                            var factory = new ChannelFactory<WebServiceAPISoap>(binding, endpoint);
+                            //从工厂获取具体的调用实例
+                            var callClient = factory.CreateChannel();
+                            setCRVPrintRequestBody Body = new setCRVPrintRequestBody();
+                            Body.strScrpit = "SELECT * from tPrintTemp_tag_FS1103 where vcOperatorID='" + loginInfo.UserId + "' order by vcInno,vcPrintcount";
+                            Body.strCRVName = file_crv + dtPrinterInfo.Rows[0]["vcReports"].ToString();
+                            Body.strPrinterName = dtPrinterInfo.Rows[0]["vcPrinter"].ToString();
+                            Body.sqlUserID = dtPrinterInfo.Rows[0]["vcSqlUserID"].ToString();
+                            Body.sqlPassword = dtPrinterInfo.Rows[0]["vcSqlPassword"].ToString();
+                            Body.sqlCatalog = dtPrinterInfo.Rows[0]["vcSqlCatalog"].ToString();
+                            Body.sqlSource = dtPrinterInfo.Rows[0]["vcSqlSource"].ToString();
+                            //调用具体的方法，这里是 HelloWorldAsync 方法
+                            Task<setCRVPrintResponse> responseTask = callClient.setCRVPrintAsync(new setCRVPrintRequest(Body));
+                            //获取结果
+                            setCRVPrintResponse response = responseTask.Result;
+                            if (response.Body.setCRVPrintResult != "打印成功")
+                            {
+                                DataRow dataRow = dtMessage.NewRow();
+                                dataRow["vcMessage"] = "标签打印失败，请联系管理员进行打印接口故障检查。";
+                                dtMessage.Rows.Add(dataRow);
+                            }
+                        }
+                        else
                         {
                             DataRow dataRow = dtMessage.NewRow();
-                            dataRow["vcMessage"] = "标签打印失败，请联系管理员进行打印接口故障检查。";
+                            dataRow["vcMessage"] = "没有标签打印机信息，请联系管理员维护。";
                             dtMessage.Rows.Add(dataRow);
                         }
                     }
@@ -219,14 +237,6 @@ namespace SPPSApi.Controllers.G11
                         return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
                     }
                     #endregion
-                    if (dtMessage != null && dtMessage.Rows.Count != 0)
-                    {
-                        //弹出错误dtMessage
-                        apiResult.code = ComConstant.ERROR_CODE;
-                        apiResult.type = "list";
-                        apiResult.data = dtMessage;
-                        return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-                    }
                     apiResult.code = ComConstant.SUCCESS_CODE;
                     apiResult.data = "打印成功";
                     return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
@@ -300,34 +310,42 @@ namespace SPPSApi.Controllers.G11
                     return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
                 }
                 #region 调用webApi打印
-                string strPrinterName = fS0603_Logic.getPrinterName("FS1103", loginInfo.UserId);
-                //创建 HTTP 绑定对象
                 string file_crv = _webHostEnvironment.ContentRootPath + Path.DirectorySeparatorChar + "Doc" + Path.DirectorySeparatorChar + "CryReports" + Path.DirectorySeparatorChar;
                 if (fS1103_Logic.getTempInfo(loginInfo.UserId, "tag").Rows.Count != 0)
                 {
-                    var binding = new BasicHttpBinding();
-                    //根据 WebService 的 URL 构建终端点对象
-                    var endpoint = new EndpointAddress(@"http://172.23.238.179/WebAPI/WebServiceAPI.asmx");
-                    //创建调用接口的工厂，注意这里泛型只能传入接口
-                    var factory = new ChannelFactory<WebServiceAPISoap>(binding, endpoint);
-                    //从工厂获取具体的调用实例
-                    var callClient = factory.CreateChannel();
-                    setCRVPrintRequestBody Body = new setCRVPrintRequestBody();
-                    Body.strCRVName = file_crv + "crv_FS1103_tag_temp.rpt";
-                    Body.strScrpit = "SELECT * from tPrintTemp_tag_FS1103 where vcOperatorID='" + loginInfo.UserId + "' order by vcInno,vcPrintcount";
-                    Body.strPrinterName = strPrinterName;
-                    Body.sqlUserID = "sa";
-                    Body.sqlPassword = "SPPS_Server2019";
-                    Body.sqlCatalog = "SPPSdb";
-                    Body.sqlSource = "172.23.180.116";
-                    //调用具体的方法，这里是 HelloWorldAsync 方法
-                    Task<setCRVPrintResponse> responseTask = callClient.setCRVPrintAsync(new setCRVPrintRequest(Body));
-                    //获取结果
-                    setCRVPrintResponse response = responseTask.Result;
-                    if (response.Body.setCRVPrintResult != "打印成功")
+                    DataTable dtPrinterInfo = fS0603_Logic.getPrinterInfo("标签", loginInfo.UserId);
+                    if (dtPrinterInfo.Rows.Count != 0)
+                    {
+                        var binding = new BasicHttpBinding();
+                        //根据 WebService 的 URL 构建终端点对象
+                        var endpoint = new EndpointAddress(dtPrinterInfo.Rows[0]["vcWebAPI"].ToString());
+                        //创建调用接口的工厂，注意这里泛型只能传入接口
+                        var factory = new ChannelFactory<WebServiceAPISoap>(binding, endpoint);
+                        //从工厂获取具体的调用实例
+                        var callClient = factory.CreateChannel();
+                        setCRVPrintRequestBody Body = new setCRVPrintRequestBody();
+                        Body.strScrpit = "SELECT * from tPrintTemp_tag_FS1103 where vcOperatorID='" + loginInfo.UserId + "' order by vcInno,vcPrintcount";
+                        Body.strCRVName = file_crv + dtPrinterInfo.Rows[0]["vcReports"].ToString();
+                        Body.strPrinterName = dtPrinterInfo.Rows[0]["vcPrinter"].ToString();
+                        Body.sqlUserID = dtPrinterInfo.Rows[0]["vcSqlUserID"].ToString();
+                        Body.sqlPassword = dtPrinterInfo.Rows[0]["vcSqlPassword"].ToString();
+                        Body.sqlCatalog = dtPrinterInfo.Rows[0]["vcSqlCatalog"].ToString();
+                        Body.sqlSource = dtPrinterInfo.Rows[0]["vcSqlSource"].ToString();
+                        //调用具体的方法，这里是 HelloWorldAsync 方法
+                        Task<setCRVPrintResponse> responseTask = callClient.setCRVPrintAsync(new setCRVPrintRequest(Body));
+                        //获取结果
+                        setCRVPrintResponse response = responseTask.Result;
+                        if (response.Body.setCRVPrintResult != "打印成功")
+                        {
+                            DataRow dataRow = dtMessage.NewRow();
+                            dataRow["vcMessage"] = "标签打印失败，请联系管理员进行打印接口故障检查。";
+                            dtMessage.Rows.Add(dataRow);
+                        }
+                    }
+                    else
                     {
                         DataRow dataRow = dtMessage.NewRow();
-                        dataRow["vcMessage"] = "标签打印失败，请联系管理员进行打印接口故障检查。";
+                        dataRow["vcMessage"] = "没有标签打印机信息，请联系管理员维护。";
                         dtMessage.Rows.Add(dataRow);
                     }
                 }
@@ -340,14 +358,6 @@ namespace SPPSApi.Controllers.G11
                     return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
                 }
                 #endregion
-                if (dtMessage != null && dtMessage.Rows.Count != 0)
-                {
-                    //弹出错误dtMessage
-                    apiResult.code = ComConstant.ERROR_CODE;
-                    apiResult.type = "list";
-                    apiResult.data = dtMessage;
-                    return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);
-                }
                 apiResult.code = ComConstant.SUCCESS_CODE;
                 apiResult.data = "打印成功";
                 return JsonConvert.SerializeObject(apiResult, Formatting.Indented, JSON_SETTING);

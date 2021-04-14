befc4aa346735b188d0b033c6a87c318352836ad
964587145@qq.com <964587145@qq.com>
2021-04-14 13:26:08 +0800

    commit

--- DataAccess/FS0618_DataAccess.cs
+++ DataAccess/FS0618_DataAccess.cs
@@ -146,8 +146,8 @@ namespace DataAccess
                 {
                     strSql.AppendLine("  and   CONVERT(varchar(10),  dOrderExportDate,112) =  '" + dOrderExportDate.Replace("-", "").Replace("/", "") + "' ");
                 }
-                //strSql.AppendLine("  order by  a.vcTargetYearMonth desc,a.vcOrderType asc,a.vcOrderNo desc,a.vcSeqno asc,a.vcPartNo desc,a.vcDock asc ");
-                strSql.AppendLine("  order by a.vcTargetYearMonth desc,a.vcSeqno asc ");
+                strSql.AppendLine("  order by  a.vcTargetYearMonth desc,a.vcOrderType asc,a.vcOrderNo desc,a.vcSeqno asc,a.vcPartNo desc,a.vcDock asc ");
+                //strSql.AppendLine("  order by a.vcTargetYearMonth desc,a.vcSeqno asc ");
                 return excute.ExcuteSqlWithSelectToDT(strSql.ToString());
             }
             catch (Exception ex)

b16df5e346504b283724a8a4cb519b83c9ad78dd
dongzhen <706587713@qq.com>
2021-04-22 09:28:09 +0800

    修改FP0019获取超期供应商邮箱代码

--- SPPSApi/Controllers/G07/FS0705Controller.cs
+++ SPPSApi/Controllers/G07/FS0705Controller.cs
@@ -217,7 +217,7 @@ namespace SPPSApi.Controllers.G07
                 string strOrderNo = fs0705_Logic.getNewOrderNo();
                 //生成发注数据
                 /*
-                 * 查询出计算结果中订单号未空的数据
+                 * 查询出计算结果中订单号为空的数据(订单号为空，说明没有生成发注数据),并且计算出的订购数量大于0(等于0不需要订购)
                  */
                 DataTable JGDT = fs0705_Logic.SCFZDataSearchComputeJG(loginInfo.BaoZhuangPlace);
                 fs0705_Logic.SCFZData(JGDT, strOrderNo);

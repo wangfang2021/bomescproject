d069a225f71b9784f9f99c662b3af2ec228e1fb4
wangjiwei <794160766@qq.com>
2021-04-29 16:24:27 +0800

    1

--- Logic/FS0406_Logic.cs
+++ Logic/FS0406_Logic.cs
@@ -72,7 +72,7 @@ namespace Logic
                 StringBuilder sbr = new StringBuilder();
                 sbr.AppendLine("<p class=\"ql-align-justify\"><strong style=\"color: black;\">FTMS &nbsp; 相关各位殿</strong></p>");
                 sbr.AppendLine("<p class=\"ql-align-justify\"><span style=\"color: black;\">&nbsp;</span></p><p class=\"ql-align-justify\"><span style=\"color: black;\">" + unit + "补给管理课&nbsp;" + strUserName + "</span></p><p class=\"ql-align-justify\"><strong>&nbsp;</strong></p>");
-                sbr.AppendLine("<p><strong style=\"color: black;\">" + unitCode + " " + range + "&nbsp;对帐数据&nbsp;</strong><strong style=\"color: blue;\">&nbsp;<u>月度对帐数</u></strong><strong style=\"color: rgb(0, 51, 204);\"><u>据</u></strong><strong style=\"color: black;\"><u>&nbsp;" + total + "</u></strong><strong style=\"color: blue;\"><u>元&nbsp;&nbsp;</u></strong><strong><u>，请</u></strong><strong style=\"color: black;\"><u>确认</u></strong><strong><u>。</u></strong></p><p class=\"ql-align-justify\"><strong>&nbsp;</strong></p>");
+                sbr.AppendLine("<p><strong style=\"color: black;\">" + unitCode + " " + range + "&nbsp;对帐数据&nbsp;</strong><strong style=\"color: black;\">&nbsp;<u>月度对帐数</u></strong><strong style=\"color: black;\"><u>据</u></strong><strong style=\"color: black;\"><u>&nbsp;" + total + "</u></strong><strong style=\"color: black;\"><u>元&nbsp;&nbsp;</u></strong><strong><u>，请</u></strong><strong style=\"color: black;\"><u>确认</u></strong><strong><u>。</u></strong></p><p class=\"ql-align-justify\"><strong>&nbsp;</strong></p>");
                 sbr.AppendLine("<p class=\"ql-align-justify\"><strong>以</strong><strong style=\"color: black;\">上</strong></p><p><br></p>");
                 string EmailBody = sbr.ToString();
 
